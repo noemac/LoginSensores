99eb9dc7f8eb330f225ebaeb8ddba844e3728340
Noelia Beatriz Machuca <nbmachuca.@gylgroup.com>
2017-09-26 01:43:35 -0300

    Commit inicial

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sensoresapp.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sensoresapp.Startup))]
+namespace sensoresapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
